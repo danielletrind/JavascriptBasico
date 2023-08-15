cf0afd9cb49ad73155cd1fe29fb37d246e036a88
Danielle Trindade <100931893+danielletrind@users.noreply.github.com>
2023-08-15 11:23:01 -0300

    Add files via upload

---
+++ Funcoes/funcoes.js
@@ -0,0 +1,10 @@
+saldo = 100000
+function dadosacesso(){
+    numcartao = prompt("Digite o número do cartão: ")
+    senha = prompt("Digite sua senha: ")
+}
+ 
+function saque(valor){
+    saldo = saldo - valor 
+    return saldo
+}
